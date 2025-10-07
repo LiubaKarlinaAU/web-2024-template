a467e993eecbf9a52ac56846c872aa2af56d38f8
LiubaKarlinaAU <pikvaforever@gmail.com>
2025-10-07 22:24:26 +0300

    Updated vite.config with repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/web-2024-template/",
   plugins: [react()],
   server: {
     host: true, // Listen on all interfaces
